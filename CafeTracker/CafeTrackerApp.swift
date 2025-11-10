ca4693d027f3a63f322a92d5fccfa9abc06ed4f6
keerthipelluru <keerthipelluru@gmail.com>
2025-11-09 22:20:18 -0600

    Initial Commit

---
+++ CafeTracker/CafeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeTrackerApp.swift
+//  CafeTracker
+//
+//  Created by Keerthi Pelluru on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CafeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CafeTrackerTests/CafeTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CafeTrackerTests.swift
+//  CafeTrackerTests
+//
+//  Created by Keerthi Pelluru on 11/9/25.
+//
+
+import Testing
+@testable import CafeTracker
+
+struct CafeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
